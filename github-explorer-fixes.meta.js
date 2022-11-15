b2f8ee4230edc0d24588b3ccfa52f5a98f3baa3b
crashmax-dev <crashmax.dev@gmail.com>
2022-11-15 12:28:16 +0000

    Deploying to gh-pages from @ crashmax-dev/github-explorer-fixes@6e20cf0e251407a623c697749d17c1c8b79f4a09 🚀

--- github-explorer-fixes.meta.js
+++ github-explorer-fixes.meta.js
@@ -3,8 +3,7 @@
 // @version     0.0.1
 // @license     MIT
 // @homepage    https://crashmax-dev.github.io/github-explorer-fixes/
-// @match       https://example.com
-// @grant       GM_addStyle
+// @match       *://github.com/*
 // @updateURL   https://crashmax-dev.github.io/github-explorer-fixes/github-explorer-fixes.meta.js
 // @downloadURL https://crashmax-dev.github.io/github-explorer-fixes/github-explorer-fixes.user.js
 // ==/UserScript==
